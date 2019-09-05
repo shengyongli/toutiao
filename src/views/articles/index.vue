18c7a84334cc9ed264ca4ce3fbbc7277b604f3a0
盛永丽 <1365125994@qq.com>
2019-09-05 19:22:27 +0800

    黑马头条PC-内容列表-页面结构-请求数据

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -14,7 +14,7 @@
         </el-radio-group>
       </el-form-item>
       <el-form-item label="频道列表：">
-        <el-select></el-select>
+        <el-select value></el-select>
       </el-form-item>
       <el-form-item label="时间选择：">
         <el-date-picker
@@ -25,12 +25,101 @@
         ></el-date-picker>
       </el-form-item>
     </el-form>
+    <!-- 内容列表结构 -->
+    <div class="total-info">共找到111111条符合条件的内容</div>
+    <div class="article-list">
+      <!-- 循环项 -->
+      <div class="article-item" v-for="(item,index) in list" :key="index">
+        <!-- 左侧内容 -->
+        <div class="left">
+          <img :src="item.cover.images.length?item.cover.images[0]:defaultImg" alt />
+          <div class="info">
+            <span class="title">{{item.title}}</span>
+            <el-tag :type=" item.status|statusType" style="width:60px">{{item.status | statusText}}</el-tag>
+            <span class="date">{{item.pubdate}}</span>
+          </div>
+        </div>
+        <!-- 右侧内容 -->
+        <div class="right">
+          <span>
+            <i class="el-icon-edit"></i>修改
+          </span>
+          <span @click="delItem(item)">
+            <i class="el-icon-delete"></i>删除
+          </span>
+        </div>
+      </div>
+    </div>
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      list: [],
+      defaultImg: require('../../assets/img/avatar.jpg')
+    }
+  },
+  methods: {
+    getArticles () {
+      this.$axios({
+        url: '/articles'
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
+  },
+  created () {
+    this.getArticles()
+  }
+}
 </script>
 
-<style>
+<style lang='less' scoped>
+.total-info {
+  border-bottom: 1px dashed #ccc;
+  padding: 15px 0;
+  color: #323745;
+  font-size: 14px;
+}
+.article-list {
+  .article-item {
+    display: flex;
+    justify-content: space-between;
+    padding: 15px 0;
+    border-bottom: 1px solid #f2f3f5;
+    .left {
+      display: flex;
+      img {
+        width: 180px;
+        height: 100px;
+        border-radius: 4px;
+      }
+      .info {
+        height: 100px;
+        display: flex;
+        flex-direction: column;
+        margin-left: 10px;
+        justify-content: space-around;
+        .title {
+          color: #333;
+          font-size: 14px;
+        }
+        .date {
+          color: #999;
+          font-size: 12px;
+        }
+      }
+    }
+    .right {
+      font-size: 12px;
+      span {
+        margin-right: 8px;
+        cursor: pointer;
+        color: #333;
+      }
+    }
+  }
+}
 </style>
