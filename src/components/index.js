763fcb5e048cf17766fe48d88455cd570b1a0adf
盛永丽 <1365125994@qq.com>
2019-09-06 23:17:01 +0800

    黑马头条PC-发布文章-封面功能组件实现

--- src/components/index.js
+++ src/components/index.js
@@ -6,12 +6,14 @@ import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 
 import { quillEditor } from 'vue-quill-editor'
+import coverImage from './publish/cover-image.vue'
 // 所有自定义组件的插件
 export default {
   install: function (Vue) {
     Vue.component('layout-aside', layoutAside) // 注册左侧导航全局组件
     Vue.component('layout-header', layoutHeader) // 注册头部全局组件
-    Vue.component('bread-crumb', breadCrumb)
-    Vue.component('quill-editor', quillEditor)
+    Vue.component('bread-crumb', breadCrumb) // 注册面包屑组件
+    Vue.component('quill-editor', quillEditor) // 注册quilleditor编辑器
+    Vue.component('cover-image', coverImage) // 注册封面图片组件
   }
 }
---
+++ src/components/publish/cover-image.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cover-image">
+    <div class="image-item" v-for="(item,index) in images" :key="index">
+      <img src="../../assets/img/pic_bg.png" alt />
+    </div>
+  </div>
+</template>
+
+<script>
+// 根据image渲染页面
+export default {
+  props: ['images']
+}
+</script>
+
+<style lang='less' scoped>
+.cover-image {
+  display: flex;
+  .image-item {
+    padding: 10px;
+    width: 200px;
+    height: 200px;
+    border: 1px solid #ccc;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
